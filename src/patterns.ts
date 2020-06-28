a65b9788db4af2a3ee990beed42c93ebccccc69a
harupy <hkawamura0130@gmail.com>
2020-06-28 22:57:05 +0900

    Add example label patterns

---
+++ src/patterns.ts
@@ -0,0 +1,4 @@
+export const BACKTICK = '- \\[(.*?)\\] ?`(.+?)`'; // matches "- [ ] `abc` ..."
+export const COLON = '- \\[(.*?)\\] ?(.+?):'; // matches "- [ ] abc: ..."
+export const ASTERISK = '- \\[(.*?)\\] ?\\*(.+?)\\*'; // matches "- [ ] *abc* ..."
+export const DOUBLE_ASTERISK = '- \\[(.*?)\\] ?\\*\\*(.+?)\\*\\*'; // matches "- [ ] **abc*** ..."
